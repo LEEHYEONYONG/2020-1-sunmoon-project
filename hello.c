89f8098d7b466f23acbab845825d95e91215a4dd
LEEHYEONYONG <dusqhfk36@naver.com>
2020-04-13 11:14:58 +0900

    Use add() function

--- hello.c
+++ hello.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+int add(int x, int y)
+{
+	return (x+y);
+}	
 int main()
 {
 	printf("Hello was d\n");
+	int a=3,b=2;
+	int c=add(3,2);
+	printf("a+b=%d\n,c);
 
 	return 0;
 }
