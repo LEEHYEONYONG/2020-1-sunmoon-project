835bf1249798129ee887e3da00114764059c0f59
LEEHYEONYONG <63217352+LEEHYEONYONG@users.noreply.github.com>
2020-05-25 02:03:34 +0900

    판매이벤트브랜치생성

--- PosSystem/src/main/MainFrame.java
+++ PosSystem/src/main/MainFrame.java
@@ -13,11 +13,14 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import sale.DealCancel;
+import sale.Payment_3;
+import sale.Payment_4;
 import sale.SaleBtn;
 import sale.SalesInputService;
 import sale.ViewSalesInput;
 
-public class MainFrame extends JFrame implements ActionListener{// 메인프레임 
+public class MainFrame extends JFrame implements ActionListener, Runnable{// 메인프레임 
 	
 	
 	public CardLayout monitor;
@@ -40,6 +43,11 @@ public class MainFrame extends JFrame implements ActionListener{// 메인프레임
 	
 	public ViewSalesInput viewSalesInput = new ViewSalesInput();
 	public SalesInputService salesInputService = new SalesInputService(this);
+	
+	// 결제및 결제취소창 프레임
+	public DealCancel dealCancel = new DealCancel();
+	public Payment_3 payment_3 = new Payment_3();
+	public Payment_4 payment_4 = new Payment_4();
 	
 
 	public MainFrame(){
@@ -134,15 +142,27 @@ public class MainFrame extends JFrame implements ActionListener{// 메인프레임
 		salebtn.sBtnPdChange.addActionListener(salesInputService);
 		salebtn.sBtnPdCancel.addActionListener(salesInputService);
 		
+//		결제3 이벤트 등록
+		payment_3.btnP3Before.addActionListener(this);
+		payment_3.btnP3Cancel.addActionListener(this);
+		payment_3.btnP3Input.addActionListener(salesInputService);
+		payment_3.btnP3Next.addActionListener(salesInputService);
 		
-		
-		
+//		결제 4 이벤트 등록
 
+		payment_4.btnP4PrintReceipt.addActionListener(this);
+		payment_4.btnP4Payment.addActionListener(salesInputService);
+
+		dealCancel.btnRefund.addActionListener(this);
+
+		viewSalesInput.code_input.addKeyListener(salesInputService);
+		viewSalesInput.product_name_input.addKeyListener(salesInputService);
 		
 		
-		
-		
-	}
+		thread = new Thread(this);
+		thread.start();
+
+	}//메인프레임메소드 끝
 	
 	public static void main(String[] args) {
 		
@@ -163,7 +183,8 @@ public class MainFrame extends JFrame implements ActionListener{// 메인프레임
 		Object ob = e.getSource();
 		Object obb =e.getActionCommand();
 		
-		if(ob==mBtnSale) {
+		//메인기능들
+		if(ob==mBtnSale) {//판매관리
 			mBtnSale.setBackground(new Color(255, 69, 0));
 			mBtnAccount.setBackground(new Color(28, 94, 94));
 			mBtnCalc.setBackground(new Color(28, 94, 94));
@@ -171,8 +192,42 @@ public class MainFrame extends JFrame implements ActionListener{// 메인프레임
 			mBtnStat.setBackground(new Color(99, 166, 166));
 			monitor.show(pMonitor, "ViewSalesInput");
 			btn.show(pFBtn, "salebtn");
+		} else if (ob == mBtnStat) {//통계
+			
+		} else if (ob == mBtnInven) {//재고관리
+			
+		} else if (ob == mBtnCalc) {
+			
 		}
 		
+		//세부기능들
+		
+		//판매관리의 기능들
+		else if(ob == salebtn.sBtnCancel) {//환불
+			dealCancel.setVisible(true);
+		}
+		else if (ob == salebtn.sBtnPdHold) {//상품보류
+			
+		} else if (ob == payment_3.btnP3Before) {
+			payment_4.setVisible(false);
+			payment_3.setVisible(false);
+
+		} else if (ob == payment_3.btnP3Cancel) {
+			payment_3.setVisible(false);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
 		
 	}
 
--- PosSystem/src/sale/Payment_4.java
+++ PosSystem/src/sale/Payment_4.java
@@ -34,6 +34,8 @@ public class Payment_4 extends JFrame{//영수증 출력 GUI
 	
 	
 	public Payment_4() {
+		
+		
 		setTitle("영수증 출력");
 		setBounds(100, 100, 600, 450);
 		contentPane = new JPanel();
@@ -42,6 +44,18 @@ public class Payment_4 extends JFrame{//영수증 출력 GUI
 		setContentPane(contentPane);
 		contentPane.add(Payment_4, BorderLayout.CENTER);
 		Payment_4.setLayout(null);
+		
+		btnP4PrintReceipt = new JButton("영수증 출력");
+		btnP4PrintReceipt.setBounds(394, 70, 150, 100);
+		Payment_4.add(btnP4PrintReceipt);
+		
+		btnP4Payment = new JButton("완료");
+		btnP4Payment.setBounds(394, 190, 150, 100);
+		Payment_4.add(btnP4Payment);
+		
+		taP4details = new JTextArea();
+		taP4details.setBounds(50, 10, 300, 360);
+		Payment_4.add(taP4details);
 		
 		
 	}
